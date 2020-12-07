186f65027fec595e67a1c5ffb24276f3a527fbb6
Cristian-Baeza <cristianbaeza092@gmail.com>
2020-12-06 21:13:11 -0700

    styled services section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,10 +1,12 @@
 import React from "react"
 import home1 from '../img/home1.png'
+//Styled 
 import styled from 'styled-components';
+import { About, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
-    <StyledAbout>
+    <About>
       <Description>
         <div className="title">
           <Hide>
@@ -23,40 +25,11 @@ const AboutSection = () => {
       <Image>
         <img src={home1} alt="random guy with camera" />
       </Image>
-    </StyledAbout>
+    </About>
   )
 }
 
 //STYLED Components
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
 
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
