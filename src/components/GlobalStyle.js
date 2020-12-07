01e0786192240f42ac45e475864086c11fe17168
Cristian-Baeza <cristianbaeza092@gmail.com>
2020-12-06 20:49:26 -0700

    AboutSection styled w styled components

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from "styled-components";
+// I can nest like in SASS if I want too 
+
+
+const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box
+}
+body{
+    background: #1b1b1b;
+}
+button{
+    font-weight: bold;
+    font-size: 1.1.rem;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 3px solid #e0b336;
+    background: transparent;
+    color: white;
+    transition: all 0.5s ease;
+    &:hover{
+        background-color: #e0b336;
+        color: white;
+    }
+}
+    h2{
+        font-weight: lighter;
+        font-size: 4rem;
+    }
+    h3{
+        color: white;
+    }
+    h4{
+        font-weight: bold;
+       
+    }
+    a{
+        font-size: 1.1rem;
+    }
+    span{
+        font-weight: bold;
+        color: #e0b336;
+    }
+    p{
+        padding: 3rem 0rem;
+        color: #ccc;
+        font-size: 1.4rem;
+        line-height: 150%;
+    }
+`;
+
+export default GlobalStyle;
