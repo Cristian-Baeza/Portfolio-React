90d035beb982d76ac02692dc7a413a557f1ebaa3
Cristian-Baeza <cristianbaeza092@gmail.com>
2020-12-06 20:19:00 -0700

    services section

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,10 +1,15 @@
 import React from "react"
 //Page Components
 import AboutSection from '../components/AboutSection';
+import ServicesSection from "../components/ServicesSection";
+
 
 const AboutMe = () => {
   return (
-    <AboutSection />
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
   )
 }
 
