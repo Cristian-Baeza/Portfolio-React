ec783277d8b1e49f00eae8ecc289531935660feb
Cristian-Baeza <cristianbaeza092@gmail.com>
2020-12-06 21:29:08 -0700

    added/styled Nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react"
 //Global Style
 import GlobalStyle from './components/GlobalStyle';
 //Import Pages
-import AboutMe from './pages/AboutMe'
+import AboutMe from './pages/AboutMe';
+import Nav from "./components/Nav";
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutMe />
     </div>
   );
