746dec0373eba2dd447dd862b9858a2cd1fff194
Cristian-Baeza <cristianbaeza092@gmail.com>
2020-12-07 19:04:07 -0700

    Added Toggle(reusable component)

---
+++ src/components/Toggle.js
@@ -0,0 +1,13 @@
+//for FAQ section
+import { useState } from "react";
+
+const Toggle = ({ children }) => {
+  const [toggle, setToggle] = useState(true);
+  return (
+    <div onClick={() => setToggle(!toggle)}>
+      {toggle ? children : ""}
+    </div>
+  )
+}
+
+export default Toggle;
